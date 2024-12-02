eedca0b879525b9b63fd4d3946a66aa562b8a9e0
serpenteno <fiacekk@gmail.com>
2024-12-02 13:56:26 +0100

    Initial commit

---
+++ Assets/Scripts/DebbugingTest.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebbugingTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int a = 5;
+        a += 5;
+        a *= 2;
+        a = 0; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
